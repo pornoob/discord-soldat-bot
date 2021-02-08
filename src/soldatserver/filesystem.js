b6e68c288f4c5a3a6c92e02b9357515c8b947322
Claudio Olivares <claudio.pol.olivares@gmail.com>
2021-02-08 19:48:21 +0000

    Improved !map command

--- src/soldatserver/filesystem.js
+++ src/soldatserver/filesystem.js
@@ -1,16 +1,22 @@
 import Config from "../config.js";
-import glob from "glob";
+
+import fs from "fs";
+import path from "path";
 
 let FileSystem = {
   maps: [],
-  set_maps(err, maps) {
-    if (err) {
-      console.log({filesyste_err: err});
-      this.maps = [];
-    } else this.maps = maps;
+  set_maps(maps) {
+    this.maps = maps.filter(file => {
+      if (file.substr(-4).toLowerCase()  !== ".pms") return false;
+      return true;
+    });
+    this.maps = this.maps.map(map => map.substr(0, map.lastIndexOf(".")));
+
+    console.log("Loaded Maps", this.maps);
   }
 };
 
-glob(`${Config.soldatserver.path}/maps/**/*.pms`, FileSystem.set_maps);
+console.log(`Looking for files on ${Config.soldatserver.path}/maps/`);
+FileSystem.set_maps(fs.readdirSync(`${Config.soldatserver.path}/maps/`));
 
 export default FileSystem;
